f82d14f98d5f9eae235088d8509cad603c1ef39e
lauratamath <laurylevi@hotmail.com>
2020-10-19 12:09:54 -0600

    método de serie d

--- serie4.cpp
+++ serie4.cpp
@@ -17,11 +17,19 @@
 
 #include <omp.h>	// Biblioteca de OpenMP 
 #include <iostream> //Biblioteca Cout
+#include <cmath> // Biblioteca de funciones matematicas
 
 #define infinite 100 //Define hasta donde realizar la sumatoria 
 
-double serie(){
-	
+double serie(int current){
+	double e = 2.178282;
+	double potencia = 2;
+	double power = pow(current, potencia); //eleva n²
+	double resultado1 = (1+1/power); //se realiza 1 + 1/n²
+	double res1 = pow(resultado1, current); // (1 + 1/n²)^n
+	resultado = e - res1; // e - (1 + 1/n²)^n
+	return resultado;
+
 }
 
 int main ()
